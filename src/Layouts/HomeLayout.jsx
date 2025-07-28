efb2be9d1a340c0c44386d78578f7145b3b66787
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-07-29 00:20:13 +0600

    add random story section on home page

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Bannner from '../Pages/Home/Banner/Bannner';
 import PackageAndGuideTabs from '../Pages/Home/PackageAndGuidTabs/PackageAndGuideTabs';
+import Story from '../Pages/Home/Story/Story';
 
 const HomeLayout = () => {
     return (
         <div>
             <Bannner></Bannner>
             <PackageAndGuideTabs></PackageAndGuideTabs>
+            <Story></Story>
         </div>
     );
 };
