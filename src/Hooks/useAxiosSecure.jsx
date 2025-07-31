e6369d666522b0fee32bbf13a2487abed5103a6c
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-08-01 02:33:25 +0600

    Change baseURL localhost url to live site url

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,8 +4,8 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
-    // baseURL: 'https://touristica-server-side.vercel.app',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://touristica-server-side.vercel.app',
+    // baseURL: 'http://localhost:5000',
     // withCredentials: true,
 })
 
