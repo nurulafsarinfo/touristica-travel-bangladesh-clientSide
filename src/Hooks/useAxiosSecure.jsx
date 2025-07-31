b44232517b95c166cd0cd31633b9026e349da4eb
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-07-31 23:44:33 +0600

    change baseURL

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,9 +4,9 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
-    // baseURL: 'https://touristica-server-side.vercel.app',
-    baseURL: 'http://localhost:5000',
-    withCredentials: true,
+    baseURL: 'https://touristica-server-side.vercel.app',
+    // baseURL: 'http://localhost:5000',
+    // withCredentials: true,
 })
 
 
